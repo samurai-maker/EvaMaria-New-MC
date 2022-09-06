0b222abbd2c382e0e7c6b0899da9b498f905f8c9
HyDrix777 <87888078+hydrix777@users.noreply.github.com>
2022-09-06 11:03:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -64,10 +64,6 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}⬇️", callback_data=f'files#{file.file_id}'
-                ),
-                ],[
-                InlineKeyboardButton(
                     text=f"♫ {file.file_name}", callback_data=f'files#{file.file_id}'
                 )
             ]
